aafcf4e84d43002e4bf69db29c40def7f5065671
frogtube <u.pradere@gmail.com>
2017-11-15 15:13:17 +0100

    Saving new form to database

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace AppBundle\Controller\Admin;
-
 use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -20,25 +19,27 @@ class GenusAdminController extends Controller
         $genuses = $this->getDoctrine()
             ->getRepository('AppBundle:Genus')
             ->findAll();
-
         return $this->render('admin/genus/list.html.twig', array(
             'genuses' => $genuses
         ));
     }
-
     /**
      * @Route("/genus/new", name="admin_genus_new")
      */
     public function newAction(Request $request)
     {
         $form = $this->createForm(GenusFormType::class);
-
         // only handles data on POST
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dump($form->getData());die();
-        }
+            $genus = $form->getData();
 
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
         return $this->render('admin/genus/new.html.twig', [
             'genusForm' => $form->createView()
         ]);
